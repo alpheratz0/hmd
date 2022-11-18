d3c09c590578d7e17e1abdc6b31b0ba9ca23d262
alpheratz0 <alpheratz99@protonmail.com>
2022-11-18 14:35:20 -0300

    format

--- popup.js
+++ popup.js
@@ -13,21 +13,24 @@ window.addEventListener('DOMContentLoaded', () => {
 				tabs[0].id,
 				{ from: 'popup', subject: 'maps' },
 				(maps) => {
-					const downloadAllBtn = document.querySelector('#download-all');
+					const downloadAllBtn =
+						document.querySelector('#download-all');
 
 					downloadAllBtn.addEventListener('click', () => {
 						const zip = new JSZip();
 						const folder = zip.folder('haxball-maps');
 
-						maps.forEach(m => {
-							folder.file(m.name.replaceAll('/', '_') + '.hbs', m.data);
+						maps.forEach((m) => {
+							folder.file(
+								m.name.replaceAll('/', '_') + '.hbs',
+								m.data
+							);
 						});
 
-						zip.generateAsync({type:"blob"})
-							.then((content) => {
-								saveAs(content, "haxball-maps.zip");
-							});
-					})
+						zip.generateAsync({ type: 'blob' }).then((content) => {
+							saveAs(content, 'haxball-maps.zip');
+						});
+					});
 
 					for (let map of maps) {
 						const li = document.createElement('li');
@@ -41,7 +44,12 @@ window.addEventListener('DOMContentLoaded', () => {
 						li.append(button);
 
 						li.addEventListener('click', () => {
-							saveAs(new Blob([map.data], {type: "text/plain;charset=utf-8"}), map.name + '.hbs');
+							saveAs(
+								new Blob([map.data], {
+									type: 'text/plain;charset=utf-8'
+								}),
+								map.name + '.hbs'
+							);
 						});
 
 						document.querySelector('#maps').append(li);
